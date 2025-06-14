b99659b56ee2bfd394bd33af93ba338d126af16f
selcukkad.r <selcukkad.r@gmail.com>
2025-06-14 22:35:41 +0000

    lifegoeson-ai

---
+++ src/bootstrap.js
@@ -0,0 +1,12 @@
+window["APP_TEMPLATE_BOOTSTRAP"] = {
+    geminiApiKey: "AIzaSyCB6lIHcFVZ0xU1lHLMxOgGoDsnhyzxC9k",
+
+    firebase: {
+      apiKey: "AIzaSyC5f3FW3o3mvhJYr2LUHzJRQiKL9vpCCDI",
+      authDomain: "lifegoeson-ai-app.firebaseapp.com",
+      projectId: "lifegoeson-ai-app",
+      storageBucket: "lifegoeson-ai-app.firebasestorage.app",
+      messagingSenderId: "674758619491",
+      appId: "1:674758619491:web:e33d58f7cc60cea1dfe356",
+    }
+    };
